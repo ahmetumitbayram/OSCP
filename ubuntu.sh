c6e6bbc5b6273d021baad537b6e99bbba74314f0
Ahmet Ümit BAYRAM <38078662+ahmetumitbayram@users.noreply.github.com>
2025-03-25 15:15:27 +0300

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,3 +1,4 @@
 sudo nft add table inet excludeTraffic
 sudo nft add chain inet excludeTraffic excludeOutgoing { type route hook output priority 0 \; policy accept \; }
 sudo nft add rule inet excludeTraffic excludeOutgoing ip daddr 192.168.1.0/24 meta mark set 0x1
+sudo sysctl -w net.ipv4.conf.all.rp_filter=2
